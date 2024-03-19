79a68d9cf3105b8f37fbcfd54d1caea02aba1aa2
e.palamarchuk <e.palamarchuk@wsmintl.com>
2024-03-19 02:57:12 +0200

    fix

--- www/layout.php
+++ www/layout.php
@@ -45,7 +45,7 @@
 	
   </head>
   <body id="page-top">
- TEST UPDATE
+
 	  <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container px-4">
